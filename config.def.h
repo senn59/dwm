04a91bad02f2faa0d6f6bc2de4f7536b9f1b129d
senn59 <senn3004@gmail.com>
2022-04-10 16:38:23 +0200

    tweaked fullgaps and added more binds for fullgaps

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -99,9 +99,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -4 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +4 } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = 0  } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = gappx } },
 	{ MODKEY,			XK_Print,  spawn,	   {.v = screencmd } },
 	{ 0, XF86XK_AudioMute, 		spawn, 		{.v = mutecmd } },
 	{ 0, XF86XK_AudioLowerVolume, 	spawn, 		{.v = voldowncmd } },
